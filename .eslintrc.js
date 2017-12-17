609edfcd1723d5badff19baf44d52cfe39f8eab5
Kim Fransman <kim.fransman@gmail.com>
2017-12-17 22:10:23 +0100

    Eslint + Prettier sitting in a tree

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,31 @@
 module.exports = {
-  parser: "babel-eslint",
-  plugins: [
-    "react",
-    "react-native",
-  ],
-  extends: [
-    "airbnb"
-  ],
+  parser: 'babel-eslint',
+  plugins: ['react', 'react-native', 'prettier', 'standard'],
+  extends: ['standard', 'prettier', 'prettier/react', 'prettier/standard'],
   globals: {
-    "__DEV__": true,
-    "__OFFLINE__": true,
-    "window": true,
-    "navigator": true,
-    "fetch": true,
-    "requestAnimationFrame": true
+    __DEV__: true,
+    __OFFLINE__: true,
+    window: false,
+    fetch: false,
+    requestAnimationFrame: false
   },
   rules: {
-    "valid-jsdoc": 2,
-    "no-var": 2,
-    "react/jsx-uses-react": 2,
-    "react/jsx-uses-vars": 2,
-    "react/jsx-no-undef": 2,
-    "react/jsx-wrap-multilines": 2,
-    "react/jsx-filename-extension": 0,
-    "comma-dangle": [2, "never"],
-    semi: [2, "never"],
-    "no-undef": 2,
-    "no-unused-vars": 2,
-    "global-require": 0,
-    "object-curly-newline": [
-      "error", { "multiline": true, "minProperties": 5 }
-    ]
+    'valid-jsdoc': 2,
+    'no-var': 2,
+    'react/jsx-uses-react': 2,
+    'react/jsx-uses-vars': 2,
+    'react/jsx-no-undef': 2,
+    'react/jsx-wrap-multilines': 2,
+    'react/jsx-filename-extension': 0,
+    'comma-dangle': [2, 'never'],
+    semi: [2, 'never'],
+    'no-undef': 2,
+    'no-unused-vars': 2,
+    'global-require': 0
   },
   settings: {
-    "import/parser": "babel-eslint",
-    "import/resolver": {
-      "babel-module": {}
+    'import/resolver': {
+      'babel-module': {}
     }
   }
 }
