933d31fb3e747340560e7fc4e16982b17fd235c2
Kim Fransman <kim.fransman@gmail.com>
2017-12-19 22:21:57 +0100

    Appcenter Xcode Stuff

--- android/app/src/main/java/com/golftracker/MainApplication.java
+++ android/app/src/main/java/com/golftracker/MainApplication.java
@@ -3,6 +3,10 @@ package com.golftracker;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
+import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
+import com.mapbox.rctmgl.RCTMGLPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +28,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appcenterCrashes_whenToSendCrashes)),
+            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appcenterAnalytics_whenToEnableAnalytics)),
+            new AppCenterReactNativePackage(MainApplication.this),
+            new RCTMGLPackage(),
             new MapsPackage()
       );
     }
